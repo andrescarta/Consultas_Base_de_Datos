5c3681de7e2b4f3c1e4e23a936ed27c38a7767c9
andres <cartaandres@hotmail.com>
2018-07-16 11:23:00 +0200

    Correccion Ejercio de consultas

--- consultasUSairlinesFlights.sql
+++ consultasUSairlinesFlights.sql
@@ -7,37 +7,44 @@ SELECT Origin, avg(ArrDelay), avg(DepDelay) FROM flights GROUP BY Origin;
 
 /* 3_CONSULTA_Retraso promedio de llegada de los vuelos, por meses y según el 
 aeropuerto origen. */
-SELECT Origin, colYear, colMonth, avg(ArrDelay) FROM flights GROUP BY Origin, colMonth;
+SELECT Origin, colYear, colMonth, AVG(ArrDelay) AS Retard	 
+FROM flights 
+GROUP BY Origin, colMonth
+ORDER BY Origin, colYear, colMonth, Retard;
+
 
 /* 4_CONSULTA_Retraso promedio de llegada de los vuelos, por meses y según el 
 aeropuerto origen (misma consulta que antes y con el mismo orden).*/
-SELECT usairports.Airport, flights.colYear, flights.colMonth, avg(flights.ArrDelay) 
+SELECT usairports.Airport, flights.colYear, flights.colMonth, avg(flights.ArrDelay) AS Retard
 FROM flights
 INNER JOIN usairports ON flights.Origin=usairports.IATA
-GROUP BY flights.Origin, flights.colMonth;
+GROUP BY colYear, usairports.Airport, flights.colMonth
+ORDER BY usairports.Airport, flights.colYear, flights.colMonth, Retard;
+
+
 
 /* 5_CONSULTA_Las compañías con más vuelos cancelados. Además, deben estar ordenadas
  de forma que las compañías con más cancelaciones aparezcan las primeras.*/
-SELECT carriers.Description, SUM(flights.Cancelled)
+SELECT carriers.Description, COUNT(flights.Cancelled) AS Cancelaciones
 FROM flights
 INNER JOIN carriers ON flights.UniqueCarrier=carriers.CarrierCode
 WHERE flights.Cancelled=1
 GROUP BY carriers.Description
-ORDER BY SUM(flights.Cancelled) DESC;
+ORDER BY Cancelaciones DESC;
 
 /* 6_CONSULTA_El identificador de los 10 aviones que más kilómetros han 
 recorrido haciendo vuelos comerciales:*/
-SELECT TailNum, SUM(AirTime)
+SELECT TailNum, SUM(Distance) AS Distance
 FROM flights
 WHERE AirTime!=0 
 GROUP BY TailNum
-ORDER BY SUM(AirTime) DESC
-LIMIT 0, 10;
+ORDER BY Distance DESC
+LIMIT 10;
 
 /* 7_CONSULTA_Compañías con su retraso promedio sólo de aquellas las 
 que sus vuelos llegan a su destino con un retraso promedio mayor de 10 minutos.*/
-SELECT UniqueCarrier, AVG(ArrDelay)
+SELECT UniqueCarrier, AVG(ArrDelay) AS Retard
 FROM flights
 GROUP BY UniqueCarrier
-HAVING AVG(ArrDelay)>10
-ORDER BY AVG(ArrDelay);
+HAVING Retard>10
+ORDER BY Retard;
